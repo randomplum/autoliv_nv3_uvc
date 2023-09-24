be10d8b1852cd7eddee55802d81d10ef55f0b361
Dominik Sliwa <dominik@sliwa.io>
2023-09-24 15:59:48 +0200

    Update to use mainline Amaranth gowin support

--- sw/gowin/board/thermalcam.py
+++ sw/gowin/board/thermalcam.py
@@ -1,64 +1,95 @@
 import os
 import subprocess
 
-from amaranth.build import *
-from amaranth.vendor.gowin_gw1n import *
-from amaranth_boards.resources import *
+from amaranth.build import Subsignal, Resource, Clock, Pins, Attrs
+from amaranth.vendor.gowin import GowinPlatform
+from amaranth_boards.resources import LEDResources, UARTResource
 
 
 __all__ = ["ThermalCamPlatform"]
 
-def FIFOResource(*args, data, clk_out, addr, sloe, slrd, slwr, pktend, full, empty, data_dir='o',
-            clk_dir='o', attrs=None, conn=None):
+
+def PSRAMResouce(*args, dq, rwds, ck, ck_n, cs_n, reset_n, attrs=None,
+                 conn=None):
+
+    io = []
+    io.append(Subsignal("dq", Pins(dq, dir="io", conn=conn, assert_width=8)))
+    io.append(Subsignal("rwds", Pins(rwds, dir="io", conn=conn,
+                                     assert_width=1)))
+    io.append(Subsignal("ck", Pins(ck, dir="o", conn=conn, assert_width=1)))
+    io.append(Subsignal("ck_n", Pins(ck_n, dir="o", conn=conn,
+                                     assert_width=1)))
+    io.append(Subsignal("cs_n", Pins(cs_n, dir="o", conn=conn,
+                                     assert_width=1)))
+    io.append(Subsignal("reset_n", Pins(reset_n, dir="o", conn=conn,
+                                        assert_width=1)))
+    if attrs is not None:
+        io.append(attrs)
+    return Resource.family(*args, default_name="psram", ios=io)
+
+
+def FIFOResource(*args, data, clk_out, addr, sloe, slrd, slwr, pktend, full,
+                 empty, data_dir='o', clk_dir='o', attrs=None, con=None):
     assert clk_dir in ('i', 'o',)
     assert data_dir in ('i', 'o', 'io',)
 
     io = []
-    io.append(Subsignal("data", Pins(data, dir="o", conn=conn, assert_width=8)))
-    io.append(Subsignal("addr", Pins(addr, dir="o", conn=conn, assert_width=2)))
-    io.append(Subsignal("clk_out", Pins(clk_out, dir=clk_dir, conn=conn, assert_width=1)))
-    io.append(Subsignal("sloe", Pins(sloe, dir="o", conn=conn, assert_width=1)))
-    io.append(Subsignal("slrd", Pins(slrd, dir="o", conn=conn, assert_width=1)))
-    io.append(Subsignal("slwr", Pins(slwr, dir="o", conn=conn, assert_width=1)))
-    io.append(Subsignal("pktend", Pins(pktend, dir="o", conn=conn, assert_width=1)))
-    io.append(Subsignal("full", Pins(full, dir="i", conn=conn, assert_width=1)))
-    io.append(Subsignal("empty", Pins(empty, dir="i", conn=conn, assert_width=1)))
+    io.append(Subsignal("data", Pins(data, dir="o", conn=con, assert_width=8)))
+    io.append(Subsignal("addr", Pins(addr, dir="o", conn=con, assert_width=2)))
+    io.append(Subsignal("clk_out", Pins(
+        clk_out, dir=clk_dir, conn=con, assert_width=1)))
+    io.append(Subsignal("sloe", Pins(sloe, dir="o", conn=con, assert_width=1)))
+    io.append(Subsignal("slrd", Pins(slrd, dir="o", conn=con, assert_width=1)))
+    io.append(Subsignal("slwr", Pins(slwr, dir="o", conn=con, assert_width=1)))
+    io.append(Subsignal("pktend", Pins(
+        pktend, dir="o", conn=con, assert_width=1)))
+    io.append(Subsignal("full", Pins(full, dir="i", conn=con, assert_width=1)))
+    io.append(Subsignal("empty", Pins(empty, dir="i", conn=con,
+                                      assert_width=1)))
     if attrs is not None:
         io.append(attrs)
     return Resource.family(*args, default_name="fifo", ios=io)
 
-class ThermalCamPlatform(GowinGW1NPlatform):
-    #device      = "GW1NR-LV9QN88PC6/I5"
-    device      = "GW1NR-UV9QN88PC6/I5"
+
+class ThermalCamPlatform(GowinPlatform):
+    part = "GW1NR-UV9QN88PC6/I5"
+    family = "GW1NR-9"
     default_clk = "clk25"
-    resources   = [
+    resources = [
         Resource("clk25", 0, Pins("52", dir="i"),
                  Clock(25e6), Attrs(IO_TYPE="LVCMOS25")),
 
-        *LEDResources(pins="86 85 84 83 82 81 80 79", attrs=Attrs(IO_TYPE="LVCMOS18")),
+        *LEDResources(pins="86 85 84 83 82 81 80 79",
+                      attrs=Attrs(IO_TYPE="LVCMOS18")),
 
         UARTResource(0,
-            rx="16", tx="15",
-            attrs=Attrs(IO_TYPE="LVCMOS25", PULLUP=1),
-            role="dce"
-        ),
+                     rx="16", tx="15",
+                     attrs=Attrs(IO_TYPE="LVCMOS18", PULLUP=1),
+                     role="dce"
+                     ),
         FIFOResource(0,
                      data="39 40 41 42 38 37 36 35", addr="28 27",
                      clk_out="25", sloe="29", slrd="19", slwr="20",
                      pktend="26", full="33", empty="32",
                      attrs=Attrs(IO_TYPE="LVCMOS33")
-        ),
+                     ),
+        PSRAMResouce(0,
+                     dq="IOL2B IOL3A IOL3B IOL4A IOL9A IOL14A IOL16A IOL17B",
+                     rwds="IOL17A", ck="IOL8A", ck_n="IOL7A", cs_n="IOL6B",
+                     reset_n="IOL2A", attrs=Attrs(IO_TYPE="LVCMOS18")
+                     ),
 
     ]
-    connectors  = [
+    connectors = [
     ]
 
     def toolchain_program(self, products, name):
         open_fpga_loader = os.environ.get("openFPGALoader", "openFPGALoader")
         with products.extract("{}.fs".format(name)) as bitstream_filename:
-            subprocess.check_call([open_fpga_loader, "-c", "tigard", "-f", bitstream_filename])
+            subprocess.check_call(
+                [open_fpga_loader, "-c", "tigard", bitstream_filename])
 
 
 if __name__ == "__main__":
-    from amaranth_boards.test.blinky import *
+    from amaranth_boards.test.blinky import Blinky
     ThermalCamPlatform().build(Blinky(), do_program=True)
--- sw/gowin/fifo_test.py
+++ sw/gowin/fifo_test.py
@@ -1,25 +1,36 @@
-from amaranth import *
+from amaranth import Elaboratable, Signal, Instance, Module, C, ClockSignal
+from amaranth import Cat
 
 
 class FIFOTest(Elaboratable):
-    def __init__(self):
 
-        self.data     = Signal(8)
-        self.addr     = Signal(2)
-        self.clk_out  = Signal()
-        self.sloe     = Signal(reset=1)
-        self.slrd     = Signal(reset=1)
-        self.slwr     = Signal(reset=1)
-        self.pktend   = Signal(reset=1)
-        self.full     = Signal()
-        self.empty    = Signal()
-        self.leds     = Signal(8)
+    def __init__(self):
+        self.data = Signal(8)
+        self.addr = Signal(2)
+        self.clk_out = Signal()
+        self.sloe = Signal(reset=1)
+        self.slrd = Signal(reset=1)
+        self.slwr = Signal(reset=1)
+        self.pktend = Signal(reset=1)
+        self.full = Signal()
+        self.empty = Signal()
+        self.leds = Signal(8)
+        self.uart_tx = Signal()
 
     def elaborate(self, platform):
         m = Module()
+        m.submodules += Instance("rPLL", p_FCLKIN="25.0", p_IDIV_SEL=0,
+                                 p_FBDIV_SEL=2, p_ODIV_SEL=8,
+                                 i_CLKIN=ClockSignal(), i_RESET=C(0),
+                                 i_RESET_P=C(0), i_CLKFB=C(0),
+                                 i_FBDSEL=C(0, 6), i_IDSEL=C(0, 6),
+                                 i_ODSEL=C(0, 6), i_PSDA=C(0, 4),
+                                 i_DUTYDA=C(0, 4), i_FDLY=C(0, 4),
+                                 o_CLKOUT=self.uart_tx)
 
         cnt = Signal(3)
         byte_cnt = Signal(range(1024))
+        frame = Signal(range(256))
         frame_size = C(256*256)
         pixel_cnt = Signal(frame_size.width)
         max_pkt = C(1024)
@@ -32,6 +43,7 @@ class FIFOTest(Elaboratable):
         m.d.comb += self.slrd.eq(1)
 
         with m.FSM() as fsm:
+            fsm
             with m.State("INIT"):
                 m.d.sync += fid.eq(0)
                 m.d.sync += eof.eq(0)
@@ -42,9 +54,9 @@ class FIFOTest(Elaboratable):
                 m.next = "START_PACKET"
             with m.State("START_PACKET"):
                 m.d.sync += self.leds.eq(0x02)
-                with m.If (byte_cnt == 0):
+                with m.If(byte_cnt == 0):
                     m.d.sync += self.data.eq(0x02)
-                with m.If (byte_cnt == 1):
+                with m.If(byte_cnt == 1):
                     m.d.sync += self.data.eq(Cat(fid, eof, C(0x20, 6)))
                 m.d.sync += self.slwr.eq(0)
                 m.d.sync += cnt.eq(cnt + 1)
@@ -54,19 +66,19 @@ class FIFOTest(Elaboratable):
             with m.State("START"):
                 m.d.sync += self.leds.eq(0x04)
                 m.d.sync += self.slwr.eq(0)
-                with m.If((max_pkt - byte_cnt) == 3 ):
+                with m.If((max_pkt - byte_cnt) == 3):
+                    m.d.sync += self.pktend.eq(0)
+                with m.If(byte_cnt % 4 == 2):
+                    m.d.sync += self.data.eq((pixel_cnt - frame) % 256)
+                with m.If(byte_cnt % 4 == 3):
+                    m.d.sync += self.data.eq(frame)
+                with m.If(byte_cnt % 4 == 0):
+                    m.d.sync += self.data.eq((pixel_cnt + frame) % 256)
+                with m.If(byte_cnt % 4 == 1):
+                    m.d.sync += self.data.eq(frame)
+                    with m.If((frame_size - pixel_cnt) == 2):
                         m.d.sync += self.pktend.eq(0)
-                with m.If (byte_cnt % 4 == 2):
-                    m.d.sync += self.data.eq(pixel_cnt % 256)
-                with m.If (byte_cnt % 4 == 3):
-                    m.d.sync += self.data.eq(0x7f)
-                with m.If (byte_cnt % 4 == 0):
-                    m.d.sync += self.data.eq(pixel_cnt % 256)
-                with m.If (byte_cnt % 4 == 1):
-                    m.d.sync += self.data.eq(0x7f)
-                    with m.If((frame_size - pixel_cnt) == 2 ):
-                        m.d.sync += self.pktend.eq(0)
-                    m.d.sync += pixel_cnt.eq( pixel_cnt + 1 )
+                    m.d.sync += pixel_cnt.eq(pixel_cnt + 1)
                 m.d.sync += cnt.eq(cnt + 1)
                 with m.If(cnt == 1):
                     m.d.sync += cnt.eq(0)
@@ -78,7 +90,7 @@ class FIFOTest(Elaboratable):
                 with m.If(cnt == 1):
                     m.d.sync += cnt.eq(0)
                     m.d.sync += byte_cnt.eq(byte_cnt + 1)
-                    with m.If(self.pktend == 0 ):
+                    with m.If(self.pktend == 0):
                         m.next = "CHECK_STATUS"
                         m.d.sync += byte_cnt.eq(0)
                         m.d.sync += self.pktend.eq(1)
@@ -89,17 +101,18 @@ class FIFOTest(Elaboratable):
                         m.d.sync += self.pktend.eq(1)
                         m.next = "START_PACKET"
             with m.State("CHECK_STATUS"):
-                    m.d.sync += self.leds.eq(0x10)
-                    with m.If((frame_size - pixel_cnt) < ((max_pkt - 4) // 2)):
-                        m.d.sync += eof.eq(1)
-                    with m.Else():
-                        m.d.sync += eof.eq(0)
-                    with m.If(pixel_cnt == 0x10000):
-                        m.d.sync += pixel_cnt.eq(0)
-                        m.d.sync += eof.eq(0)
-                        m.d.sync += fid.eq( ~fid )
-                        m.d.sync += self.leds.eq(0x20)
-                    with m.If(self.full == 1):
-                        m.next = "START_PACKET"
+                m.d.sync += self.leds.eq(0x10)
+                with m.If((frame_size - pixel_cnt) < ((max_pkt - 4) // 2)):
+                    m.d.sync += eof.eq(1)
+                with m.Else():
+                    m.d.sync += eof.eq(0)
+                with m.If(pixel_cnt == 0x10000):
+                    m.d.sync += pixel_cnt.eq(0)
+                    m.d.sync += eof.eq(0)
+                    m.d.sync += fid.eq(~fid)
+                    m.d.sync += frame.eq(frame + 1)
+                    m.d.sync += self.leds.eq(0x20)
+                with m.If(self.full == 1):
+                    m.next = "START_PACKET"
 
         return m
